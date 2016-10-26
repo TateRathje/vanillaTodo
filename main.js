dd44758d68b9069923c530bb38f7f66b29db4199
Tate Rathje <taterathje@gmail.com>
2016-10-26 02:33:42 -0700

    Added new js files and started setting up main.js code.

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = app || {};
+
+app.main = (function() {
+
+	var elements = {
+		noteField : document.querySelector('.write-note'),
+		noteSubmit : document.querySelector('.submit-note'),
+		noteList : document.querySelector('.notes'),
+		noNotesFound : document.querySelector('.no-notes-found')
+	};
+
+})();
