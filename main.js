e0e5f2b470d47daffe25db259dfb5daf357b3aae
Tate Rathje <taterathje@gmail.com>
2016-10-27 16:35:25 -0700

    Started building out Model and View constructors.

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ app.main = (function() {
 
 	var addAsFirstChild = function(parent, child) {
 		var parentNode = parent,
-			  childNode = child;
+			childNode = child;
 		if (parentNode.firstChild) {
 			parentNode.insertBefore(child, parent.firstChild);
 		} else {
@@ -25,21 +25,62 @@ app.main = (function() {
 		}
 	};
 
+	var View = function (note, containerEl) {
+		
+		var index = notes.indexOf(note),
+				that = this;
+
+				this.render = function () {
+					// body...
+				};
+
+				this.like = function () {
+					// body...
+				};
+
+				this.remove = function () {
+					// body...
+				};
+
+				this.attachEvents = function () {
+					// body...
+				};
+
+				this.init = function () {
+					// body...
+				};	 
+
+	};
+
 	var Model = function(noteData, collection) {
+
+		/*noteData = {
+			noteBodyText: 'blah blah',
+			liked: false
+		}*/
 
 		this.data = noteData;
 
 		this.save = function() {
 			collection.push(this.data);
+			localStorage.setItem('notes', JSON.stringify(collection));
+			return this;
 		};
 
 		this.like = function() {
-
+			this.data.liked = !this.data.liked;
+			var indexToUpdate = collection.indexOf(this.data);
+			collection.splice(indexToUpdate, 1, this.data);
+			localStorage.setItem('notes', JSON.stringify(collection));
+			return this;
 		};
 
-		this.remove = function () {
-			
-		}
+		this.remove = function() {
+			var indexToRemove = collection.indexOf(this.data);
+			collection.splice(indexToRemove, 1);
+			localStorage.setItem('notes', JSON.stringify(collection));
+			return this;
+		};
 
 	};
 
